79140fc756c794873b49d0b6758e8f29f07ecbae
masahiro0720 <YourEmailAddress>
2025-11-28 11:57:00 +0000

    fifth

--- RTC/RTMtoROS/build/include/RTMtoROS/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/workspace/OpenRTM_ROS_interoperability_system2/RTC/RTMtoROS/include/RTMtoROS
-# Build directory: /root/workspace/OpenRTM_ROS_interoperability_system2/RTC/RTMtoROS/build/include/RTMtoROS
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
